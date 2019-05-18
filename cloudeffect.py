92ba43aba49486807d6e739ed178040ece2f49f9
SilvanKohler <edu.silvan.kohler@gmail.com>
2019-05-18 14:24:52 +0200

    update

--- cloudeffect.py
+++ cloudeffect.py
@@ -22,10 +22,11 @@ def cloudEffect(width, height, x, y):
     output = np.reshape(output, (height,width))
     return output
 
-for x in np.arange(0,40):
-    for line in cloudEffect(30, 30, x, 0):
-        for p in line:
-            print('  ' if p < 4 else 'XX' if p < 8 else 'MM', end='')
-        print('')
-    time.sleep(0.1)
-    os.system('cls')
+if __name__ == '__main'
+    for x in np.arange(0,40):
+        for line in cloudEffect(30, 30, x, 0):
+            for p in line:
+                print('  ' if p < 4 else 'XX' if p < 8 else 'MM', end='')
+            print('')
+        time.sleep(0.1)
+        os.system('cls')
